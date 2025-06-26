6ff27bdf494b7a7894500e2480c5bea5f550b217
pp <15078584+pp_luo@user.noreply.gitee.com>
2025-06-26 20:46:50 +0800

    数据库连接成功，然后修改了依赖关系

---
+++ teacher-service/src/test/java/org/example/teacherservice/TestConnectionWithDataBase.java
@@ -0,0 +1,25 @@
+package org.example.teacherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class TestConnectionWithDataBase {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void testConnection() throws SQLException {
+        try (Connection conn = dataSource.getConnection()) {
+            assertNotNull(conn);
+            System.out.println("数据库连接成功：" + conn.getMetaData().getURL());
+        }
+    }
+}
